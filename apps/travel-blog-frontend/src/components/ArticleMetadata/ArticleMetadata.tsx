dae7dd05225b3d67bae0f4f3b65432b0b7a274d2
Jakub Jaroš <jakub.jaros@ataccama.com>
2024-02-26 16:41:15 +0100

    travel-blog-frontend: fix build

--- apps/travel-blog-frontend/src/components/ArticleMetadata/ArticleMetadata.tsx
+++ apps/travel-blog-frontend/src/components/ArticleMetadata/ArticleMetadata.tsx
@@ -6,14 +6,14 @@ import { Post } from '@contentlayer/generated'
 
 export const ArticleMetadata: React.FC<{ article: Post }> = ({ article }) => {
   return null
-  return (
-    <Inline align="space-between">
-      <Inline.Item>
-        <Text variant="caption">{article.author.data.attributes?.name}</Text>
-      </Inline.Item>
-      <Inline.Item>
-        <Text variant="caption">{formatISODate(article.publishedAt, 'MMM d, yyyy')}</Text>
-      </Inline.Item>
-    </Inline>
-  )
+  // return (
+  //   <Inline align="space-between">
+  //     <Inline.Item>
+  //       <Text variant="caption">{article.author.data.attributes?.name}</Text>
+  //     </Inline.Item>
+  //     <Inline.Item>
+  //       <Text variant="caption">{formatISODate(article.publishedAt, 'MMM d, yyyy')}</Text>
+  //     </Inline.Item>
+  //   </Inline>
+  // )
 }
